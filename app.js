68de12da916349fcfcb999bbf9c66f1a77912f44
aadesh <aadeshrn@gmail.com>
2023-07-25 23:20:51 +0530

    Ep 1 : Inception

---
+++ app.js
@@ -0,0 +1,15 @@
+const heading = React.createElement('div',{id:'parent'},
+    React.createElement('div',{id:'child'},
+        [
+            React.createElement('h3',{id:'child1'},'Hello World from React child1!!!'),
+            React.createElement('h3',{id:'child2'},'Hello World from React child2!!!'),
+            React.createElement('h3',{id:'child3'},'Hello World from React child3!!!')
+        ]
+    )
+);
+
+console.log("heading : ",heading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+        
+root.render(heading); //render method converts object to html element
